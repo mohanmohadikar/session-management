a3f82bb77accd86d1101c23d10b3959b62cd7671
mohanmohadikar <mohanmmohadikar@gmail.com>
2024-11-23 21:03:36 +0530

    initial commit

---
+++ src/test/java/com/mohanmmohadikar/session_management/SessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohanmmohadikar.session_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
